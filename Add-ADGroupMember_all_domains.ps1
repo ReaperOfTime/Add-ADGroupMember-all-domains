4510313c6da9b4275f5d9e7c8c9d0ea6bada6fc0
ReaperOfTime <137084844+ReaperOfTime@users.noreply.github.com>
2023-06-19 17:06:33 +0200

    Add files via upload
    This script was created for the purpose of bulk add users from different AD domains in the same forest to one group (located in different domain than users). I did not find anything consistent online so I created this - maybe it will help someone one day. I'm using this for daily work and in automated processes in task scheduler so I find it quite reliable.
    By design, it will return a lot errors in the background while checking SamAccountName against all domain controllers and it will run for a long time, depending on how many users you have times how many domain controllers you checking then against.
    Please note that this was created for a highly specific scenario.
    CSV format (when user email = UPN):
    "UserPrincipalName"
    "User1@email.com"
    "User2@email.com"
    "User3@email.com"
    
    Any suggestions are welcome. Primarily I'd like this to be faster without any errors. But how I wrote above - I did not find anything else that would be reliable and work as expected.

---
+++ Add-ADGroupMember_all_domains.ps1
@@ -0,0 +1,48 @@
+﻿# Start logging
+Start-Transcript -Path "<path_to_log_file>"
+
+# Error action handeling
+$ErrorActionPreference = "Continue"
+
+# Dynamic group name
+$ADGroupname = Get-ADGroup "<GroupName>" -Server czbortechvdc001.kktech.local
+
+# Input path
+$inputupn = "<path_to_input_csv>"
+
+# Get Domain in Active Directory Forest
+$Domains = (Get-ADForest).Domains
+
+# Get Domain Controller list
+$DClist = ForEach ($Domain in $Domains) {
+Get-ADDomainController -DomainName $Domain -Discover -Service PrimaryDC | Select -ExpandProperty hostname
+    }
+
+# Import csv file with email addreses, convert to SamAccountName and add to group
+foreach ($line in Import-Csv $inputupn)
+{
+    if([string]::IsNullOrWhiteSpace($line.UserPrincipalName))
+    {
+        Write-Warning 'Empty UserPrincialName Value:'
+        Write-Warning $line
+        continue
+    }
+
+    $adusers = foreach ($DC in $DClist){ Get-ADUser -Filter "UserPrincipalName -eq '$($line.UserPrincipalName)'" -Server $DC }
+    if(-not $adusers)
+    {
+        Write-Warning "$($line.UserPrincipalName) could not be found."
+        continue
+    }
+
+    # Input check - uncomment to see progress (errors) when running from terminal. When using in scheduled tasks comment.
+    # Write-Host 'The following users will be added to the Group:' $adusers.samaccountname
+
+    foreach ($aduser in $adusers.samaccountname)
+    {
+        Set-ADGroup $ADGroupname -Add @{member = $adusers.DistinguishedName}
+    }
+}
+
+# Stop logging
+Stop-Transcript
